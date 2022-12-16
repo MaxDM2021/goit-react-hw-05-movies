520b65fb9c7193da2e64f8bbc4f5b99c97e535a9
MaxDM2021 <95755128+MaxDM2021@users.noreply.github.com>
2022-12-16 22:58:27 +0000

    Deploying to gh-pages from @ MaxDM2021/goit-react-hw-05-movies@867f62c392653d7b9db5a2596507bab799fb7aaf 🚀

---
+++ static/js/736.8e3c01c9.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[736],{2736:function(e,t,r){r.r(t);var a=r(184);t.default=function(){return(0,a.jsx)("div",{children:" Reviews"})}}}]);
+//# sourceMappingURL=736.8e3c01c9.chunk.js.map
