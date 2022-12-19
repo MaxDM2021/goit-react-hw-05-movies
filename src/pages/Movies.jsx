cafa221ac838e964a1a8fa8591fd5c4f686930a9
Maksym Dmitriiev <95755128+MaxDM2021@users.noreply.github.com>
2022-12-19 20:33:28 +0300

    232

--- src/pages/Movies.jsx
+++ src/pages/Movies.jsx
@@ -12,30 +12,53 @@ const Movies = () => {
   const [movieName, setName] = useState('');
   const [page, setPage] = useState(1);
   const [movies, setMovies] = useState([]);
+  // const [searchParams, setSearchParams] = useSearchParams();
+
+  console.log(movieName)
+
   const [total, setTotal] = useState(0);
 
-  const handleNameChange = event => {
+  function handleNameChange (event)  {
     setName(event.target.value.toLowerCase());
+    
+   
   };
 
   const handleSubmit = event => {
     event.preventDefault();
 
-
-
     if (movieName.trim() === '') {
       return toast.error('Введите название изображения!');
     }
     setPage(1);
-    setName(movieName);
+    setName(event.target.value.toLowerCase());
     setName('');
+  
   };
 
+
+//   const handleSubmit = (event) => {
+//     event.preventDefault();
+
+//        if (movieName.trim() === '') {
+//       return toast.error('Введите название изображения!');
+//     }
+
+//     setName(event.target.name.value.toLowerCase().trim());
+//     setPage(1);
+//     setName('');
+// }
+
+// const handleNameChange =(name)=> {
+//     const nextParams = name !== '' ? { name } : {};
+//     setSearchParams(nextParams);
+// }
+
   useEffect(() => {
-    if (movieName.trim()) {
+    if (movieName) {
     fetchMovieQuery(movieName, page, total).then(setMovies);
     }
-    //eslint-disable-next-line
+    //eslint-disable-next-line react-hooks/exhaustive-deps
   }, [page, movieName]);
 
   // Как работает useEffect: Если hitName изменятся, запустятся все фунции вложенные в useEffect
@@ -55,8 +78,8 @@ const Movies = () => {
       {movies.length > 0 && (
         <ul>
           {movies.map(movie => (
-            <li key={movie.id}>
-              <Link to={`${movie.id}`} state={{ from: location }}>
+            <li key={movie.id} movie={movie}>
+              <Link to={`${movie.id}`} state={{ from: location }  }>
                 {movie.title}
               </Link>
             </li>
